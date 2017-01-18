7ff461a387876df664f8d208d2a928a885a7cd04
Fabio Teixeira <fabio1079@gmail.com>
2017-01-18 10:06:21 -0200

    Add navigation drawer (Android exclusive)
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- components/app.js
+++ components/app.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  DrawerLayoutAndroid
 } from 'react-native';
 
 import { Scene, Router } from 'react-native-router-flux';
@@ -10,22 +11,53 @@ import { Scene, Router } from 'react-native-router-flux';
 import MainMenu from './main-menu';
 import FgaNews from './fga-news';
 import Events from './events';
-import MoreMenu from './more-menu';
+import DrawerMenu from './drawer-menu';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      drawerIsOpened: false
+    }
+
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.closeDrawer = this.closeDrawer.bind(this);
+  }
+
+  toggleDrawer() {
+    if (this.state.drawerIsOpened) {
+      this.refs['DRAWER'].closeDrawer();
+    } else {
+      this.refs['DRAWER'].openDrawer();
+    }
+  }
+
+  closeDrawer() {
+    this.refs['DRAWER'].closeDrawer();
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.mainContentContainer}>
-          <Router>
-            <Scene key="FgaNews" component={FgaNews} title='News' hideNavBar={true} />
-            <Scene key="Events" component={Events} title='Events' hideNavBar={true} />
-            <Scene key="MoreMenu" component={MoreMenu} title='More opstions' hideNavBar={true} />
-          </Router>
+          <DrawerLayoutAndroid
+            drawerWidth={300}
+            ref={'DRAWER'}
+            drawerPosition={DrawerLayoutAndroid.positions.Right}
+            onDrawerClose={() => { this.setState({drawerIsOpened: false}) }}
+            onDrawerOpen={() => { this.setState({drawerIsOpened: true}) }}
+            renderNavigationView={() => <DrawerMenu />}>
+              {/* Main content goes here */}
+              <Router>
+                <Scene key="FgaNews" component={FgaNews} title='News' hideNavBar={true} />
+                <Scene key="Events" component={Events} title='Events' hideNavBar={true} />
+              </Router>
+            </DrawerLayoutAndroid>
         </View>
 
         <View style={styles.mainMenuContainer}>
-          <MainMenu />
+          <MainMenu toggleDrawerMenu={this.toggleDrawer} closeDrawerMenu={this.closeDrawer} />
         </View>
       </View>
     );
