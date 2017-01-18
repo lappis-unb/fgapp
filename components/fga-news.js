9ca1be4ed99f6a325c87ab263006c154bcb18ed9
Fabio Teixeira <fabio1079@gmail.com>
2017-01-18 15:59:07 -0200

    Do some refactory on news
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- components/fga-news.js
+++ components/fga-news.js
@@ -4,8 +4,12 @@ import {
   View,
   Text,
   ListView,
-  TouchableOpacity
+  TouchableOpacity,
+  ActivityIndicator
 } from 'react-native';
+
+import { ArticleService } from '../services';
+import NewsListItem from './news-list-item';
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
@@ -14,7 +18,8 @@ export default class FgaNews extends Component {
     super(props);
 
    this.state = {
-      dataSource: ds.cloneWithRows([]) // just to have 10 rows on the prototype
+      dataSource: ds.cloneWithRows([]),
+      loadingArticles: true
     }
   }
 
@@ -27,80 +32,36 @@ export default class FgaNews extends Component {
   }
 
   componentDidMount() {
-    let apiUrl = "https://fga.unb.br/";
-    let apiNamespace = "api/v1/";
-    fetch(apiUrl + apiNamespace + 'articles?parent_id=46')
-      .then((response) => response.json())
-        .then((responseJson) => {
-          this.setState({
-            dataSource: ds.cloneWithRows(responseJson.articles)
-          });
+    ArticleService.get(46)
+      .then(response => response.data)
+      .then((response) => {
+        this.setState({
+          dataSource: ds.cloneWithRows(response.articles),
+          loadingArticles: false
         });
+      });
   }
 
   render() {
+    let content = "";
+
+    if (this.state.loadingArticles) {
+      content = <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                  <ActivityIndicator size={60} color="#21ba57" />
+                </View>
+
+    } else {
+      content = <ListView
+                  dataSource={this.state.dataSource}
+                  renderRow={(rowData) => this.buildRowData(rowData)}
+                  enableEmptySections={true}
+                />
+    }
+
     return (
       <View style={{flex: 1}}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => this.buildRowData(rowData)}
-          enableEmptySections={true}
-        />
+        {content}
       </View>
-    );
-  }
-}
-
-
-// Component here just for prototype simplification
-class NewsListItem extends Component {
-  constructor(props) {
-    super(props);
-
-    const date = new Date(props.date),
-        locale = "pt",
-        month = date.toLocaleString(locale, { month: "long" });
-
-    this.state = {
-      day: date.getDate(),
-      month: month.toLocaleUpperCase().split(' ')[1].substr(0, 3)
-    }
-  }
-
-  goToArticle() {
-    Actions.Article({
-      title: this.props.title,
-      body: this.props.body,
-      date: this.props.date
-    });
-  }
-
-  render() {
-    return (
-      <TouchableOpacity onPress={() => this.goToArticle()}>
-        <View style={{flex: 1, backgroundColor: '#e4e4e4' ,flexDirection: 'row', height: 70, margin: 5, marginBottom: 15, padding: 5}}>
-          <View style={{flex: 1, alignItems: 'center', backgroundColor: '#ffffff', marginRight: 5}}>
-            <View style={{flex: 1}}>
-              <Text style={{color: '#000000', fontSize: 18}}>{this.state.day}</Text>
-            </View>
-
-            <View style={{flex: 1}}>
-              <Text style={{color: '#000000'}}>{this.state.month}</Text>
-            </View>
-          </View>
-
-          <View style={{flex: 5, justifyContent: 'center'}}>
-            <View style={{flex: 1}}>
-              <Text numberOfLines={1} style={{color: '#000000', fontWeight: '800', fontSize: 18}}>{this.props.title}</Text>
-            </View>
-
-            <View style={{flex: 2, marginTop: 5}}>
-              <Text numberOfLines={2} style={{color: '#444', fontSize: 12}}>{this.props.summary}</Text>
-            </View>
-          </View>
-        </View>
-      </TouchableOpacity>
-
     );
   }
 }
