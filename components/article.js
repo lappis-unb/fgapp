95026aa80aca7a8f61a753acfdca8927739613e5
Vitor Barbosa <vitornga15@gmail.com>
2017-01-18 11:56:12 -0200

    Styling news page with escaped html

--- components/article.js
+++ components/article.js
@@ -3,22 +3,38 @@ import {
   View,
   StyleSheet,
   Text,
-  WebView
+  WebView,
+  Linking
 } from 'react-native';
 
 export default class Article extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.title}>
+        <View style={styles.titleContainer}>
           <Text style={styles.title}>{this.props.title}</Text>
         </View>
 
-        <View style={styles.date}>
+        <View style={styles.dateContainer}>
           <Text style={styles.date}>{this.props.date}</Text>
         </View>
 
-        <Text style={styles.body}>{this.props.body}</Text>
+        <View style={styles.bodyContainer}>
+          <WebView
+            source={{html: this.props.body}}
+            ref={(ref) => { this.webview = ref }}
+            onNavigationStateChange={(e) => {
+              if(e.url != this.props.body) {
+                Linking.canOpenURL(e.url).then(supportedLink => {
+                  if(supportedLink) {
+                    this.webview.stopLoading();
+                    Linking.openURL(e.url);
+                  }
+                })
+              }
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -30,20 +46,28 @@ const styles = StyleSheet.create({
     paddingLeft: 20,
     paddingRight: 20
   },
+
+  titleContainer: {
+    flex: 7,
+    justifyContent: "center",
+  },
+
   title: {
     color: "#005263",
-    flex: 2,
-    fontSize: 32,
+    fontSize: 20,
     fontWeight: "bold",
-    flexDirection: "row",
-    alignItems: "center",
+    textAlign: "center"
   },
-  body: {
-    flex: 17,
-    fontSize: 18
+
+  dateContainer: {
+    flex: 2,
   },
+
   date: {
-    flex: 1,
-    textAlign: "right",
+    textAlign: "right"
+  },
+
+  bodyContainer: {
+    flex: 17
   }
 })
