e72e48b9e38e3d2d772f458349f27e3b2ef3f72f
DylanGuedes <djmgguedes@gmail.com>
2017-01-23 14:35:57 -0200

    Removes professors component, remove unused tests, rename tests imports

--- __tests__/drawer-menu.js
+++ __tests__/drawer-menu.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import DrawerMenu from '../components/drawer-menu.js';
+import DrawerMenu from '../js/components/drawer-menu.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
