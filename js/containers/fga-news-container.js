a79db04ff955e8bf6ca7f1a09e387ffb39c62f27
Vitor Barbosa <vitornga15@gmail.com>
2017-01-25 16:04:56 -0200

    Refactoring article image parser and adding check for article author name
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>
    Signed-off-by: Vitor Barbosa <vitornga15@gmail.com>

--- js/containers/fga-news-container.js
+++ js/containers/fga-news-container.js
@@ -22,7 +22,7 @@ const mapDispatchToProps = (dispatch) => ({
           body: article.body,
           title: article.title,
           created_at: article.created_at,
-          authorName: article.setting.author_name
+          authorName: article.setting ? "por " + article.setting.author_name : ""
         }));
 
         const link = response.headers.link;
