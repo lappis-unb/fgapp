d01fbe9a27f9891d2b85ab972af82e727e9841ec
Daniel Henrique <danielhmarinho@gmail.com>
2017-03-29 15:05:37 -0300

    basic buttons and linking method

--- js/components/links.js
+++ js/components/links.js
@@ -1,12 +1,53 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, Linking, TouchableOpacity, StyleSheet} from 'react-native';
 
 export default class Links extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Links</Text>
-      </View>
-    );
-  }
+    static propTypes = {
+        url: React.PropTypes.string
+    };
+
+    clickButton = (url) => {
+        Linking.canOpenURL(url).then(supported => {
+            if (supported) {
+                Linking.openURL(url);
+            } else {
+                console.log('Don\'t know how to open URI: ' + url);
+            }
+        });
+    };
+
+    render() {
+        return (
+          <View>
+            <TouchableOpacity onPress={() => {this.clickButton('https://facebook.com')}}>
+                <View style={styles.button}>
+                    <Text style={styles.text}> Facebook </Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {this.clickButton('https://facebook.com')}}>
+                <View style={styles.button}>
+                    <Text style={styles.text}> Open </Text>
+                </View>
+            </TouchableOpacity>
+          </View>
+
+        );
+    }
 }
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        padding: 10,
+        paddingTop: 30
+    },
+    button: {
+        padding: 10,
+        backgroundColor: '#3B5998',
+        marginBottom: 10
+    },
+    text: {
+        color: 'white'
+    }
+});
