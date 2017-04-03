5942e4032bfd847f16e5e628b9d71a0430aedfd9
Daniel Henrique <danielhmarinho@gmail.com>
2017-04-03 15:32:05 -0300

    adding buttons

--- js/components/links.js
+++ js/components/links.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Text, View, Linking, TouchableOpacity, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Links extends Component {
     static propTypes = {
@@ -18,17 +19,42 @@ export default class Links extends Component {
 
     render() {
         return (
-          <View>
-            <TouchableOpacity onPress={() => {this.clickButton('https://facebook.com')}}>
-                <View style={styles.button}>
-                    <Text style={styles.text}> Facebook </Text>
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => {this.clickButton('https://facebook.com')}}>
-                <View style={styles.button}>
-                    <Text style={styles.text}> Open </Text>
-                </View>
-            </TouchableOpacity>
+          <View style={{backgroundColor:"#cdcdcd",  flex: 1}} >
+            <View style={styles.button}>
+              <Icon.Button name="home" color="#146d43" backgroundColor="#fff" onPress={() => {this.clickButton('http://unb.br')}}>
+                 Portal da UnB
+              </Icon.Button>
+            </View>
+            <View style={styles.button}>
+              <Icon.Button name="graduation-cap" color="#146d43" backgroundColor="#fff" onPress={() => {this.clickButton('https://matriculaweb.unb.br/')}}>
+                 Matrícula Web
+              </Icon.Button>
+            </View>
+            <View style={styles.button}>
+              <Icon.Button name="handshake-o" color="#146d43" backgroundColor="#fff" onPress={() => {this.clickButton('https://www.saeweb.unb.br/')}}>
+                 SaeWeb
+              </Icon.Button>
+            </View>
+            <View style={styles.button}>
+              <Icon.Button name="book" color="#146d43" backgroundColor="#fff" onPress={() => {this.clickButton('http://aprender.unb.br/')}}>
+                 Moodle UnB
+              </Icon.Button>
+            </View>
+            <View style={styles.button}>
+              <Icon.Button name="book" color="#146d43" backgroundColor="#fff" onPress={() => {this.clickButton('http://gama.ead.unb.br/')}}>
+                 Moodle FGA
+              </Icon.Button>
+            </View>
+            <View style={styles.button}>
+              <Icon.Button name="file-text-o" color="#146d43" backgroundColor="#fff" onPress={() => {this.clickButton('https://unbdoc.unb.br/')}}>
+                 UnB Doc
+              </Icon.Button>
+            </View>
+            <View style={styles.button}>
+              <Icon.Button name="cutlery" color="#146d43" backgroundColor="#fff" onPress={() => {this.clickButton('http://www.ru.unb.br/cardapio-ru-unb')}}>
+                 Cardápio RU
+              </Icon.Button>
+            </View>
           </View>
 
         );
@@ -36,16 +62,10 @@ export default class Links extends Component {
 }
 
 var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        padding: 10,
-        paddingTop: 30
-    },
     button: {
-        padding: 10,
-        backgroundColor: '#3B5998',
-        marginBottom: 10
+        padding: 5,
+        justifyContent: 'center',
+        alignItems: 'stretch'
     },
     text: {
         color: 'white'
